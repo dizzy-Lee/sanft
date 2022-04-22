35d311c1d63abf9ea6551815e5b65d9badc7dadb
Yifan <kumasama5666@gmail.com>
2022-04-22 18:16:30 +0800

    配置基础样式，编写button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react"
 
+import SanButton, { ButtonType, ButtonSize } from "./components/SanButton/button"
 function App() {
-  return (
-    <div className="App">
-      <button>click me!</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <SanButton>Hello world</SanButton>
+            <SanButton
+                btnType={ButtonType.Primary}
+                size={ButtonSize.Large}
+                disabled
+            >
+                large
+            </SanButton>
+            <SanButton
+                btnType={ButtonType.Link}
+                href="https://www.baidu.com"
+                size={ButtonSize.Large}
+                disabled
+            >
+                link
+            </SanButton>
+        </div>
+    )
 }
 
-export default App;
+export default App
